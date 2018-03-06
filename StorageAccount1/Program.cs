20beb9ea4fdd41d6d0729a0daef202e861101f90
pavigowdaNS <pavi1988gowda@gmail.com>
2018-03-06 16:54:59 +0530

    fgdf
    fff

---
+++ StorageAccount1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.Azure;
+
+namespace StorageAccount1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAc = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+           CloudBlobClient blobClient= storageAc.CreateCloudBlobClient();
+           CloudBlobContainer blobcontainer= blobClient.GetContainerReference("mycontainer");
+            blobcontainer.CreateIfNotExists();
+           
+            CloudBlockBlob blockblob = blobcontainer.GetBlockBlobReference("myblob");
+            blobcontainer.SetPermissions(
+           new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+       
+                using (var fileStream = System.IO.File.OpenRead(@"C:\Users\AGSPL14\Desktop\pa.jpg"))
+            {
+                blockblob.UploadFromStream(fileStream);
+            }
+
+        }
+    }
+}
